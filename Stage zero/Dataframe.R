4f761d2bddee54fb4f2f97989e8a64edadfea04e
Tomi-beloved <oluwaseuntomilayo9@gmail.com>
2025-02-08 22:39:26 +0100

    Update and rename Codes to Dataframe.R

---
+++ Stage zero/Dataframe.R
@@ -0,0 +1,14 @@
+#creating a list of information for start codon team
+my_team <- data.frame(
+   names = c("Akinjide Anifowose", "Tomilayo Fadairo"), 
+   slack_username = c("Sam", "TomiDbeloved"), 
+   email = c("anifowak@yahoo.com", "Oluwaseuntomilayo9@gmail.com"),
+   hobby = c("Studying", "Reading"),
+   countries = c("United Kingdom", "Nigeria"),
+   Discipline = c("Molecular Cell Biology", "Neurodegenerative disorder"),
+   preferred_programming_language = c("R", "R")
+)
+
+#print the information of the tesm
+print(my_team)
+dim(my_team)
